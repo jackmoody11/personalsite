921428fa312c9e5411ed0a602535af5cee8da0fc
jackmoody11 <jackmoody@unc.edu>
2018-10-08 18:49:15 -0400

    Add basic styling framework

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,10 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
-def post_detail(request):
+def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def about(request):
+    return render(request, 'blog/about.html', {})
